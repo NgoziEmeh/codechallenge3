7c83209200628c3fd3b7eaaf024baa9cfb36aff0
Ngozi Emeh-Festus <35117703+NgoziEmeh@users.noreply.github.com>
2019-03-05 22:29:37 +0100

    Create solution.js

---
+++ solution.js
@@ -0,0 +1,8 @@
+var john = {
+    fullName: 'John Smith',
+    mass: 81,  //in kg
+    height: 1.7,  //in meters
+    calcBMI: function(){
+        return this.mass/(this.height * this.height);
+    }
+};
